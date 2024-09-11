089ff30078573ed26c4e02a6fa35d2b8bde19947
suchitha-1007 <suchitha.dadige@gmail.com>
2024-09-11 10:31:54 +0530

    message

---
+++ index.js
@@ -0,0 +1,26 @@
+let accordian = document.getElementsByClassName("FAQ__title");
+
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+function hidePreloder(){
+  document.getElementById("preloder").style.display="none";
+}
+document.getElementById("preloder").style.display="block";
+
+setTimeout(hidePreloder,3050);
